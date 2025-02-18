9089dac9ae3bcfab3c6bce30e9770f42bf87b4d4
FazekasBalint <fazekas.balint@students.jedlik.eu>
2025-02-18 20:44:41 +0100

    Regisztráció folytatva

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('bands', BandsController::class);
 
-Route::post('/register',[AuthController::class,'reqister']);
+Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/logout',[AuthController::class,'logout']);
 
